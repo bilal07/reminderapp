c8fa4ee33c326b745a1cec93fde9ab0005842f1f
bilal07 <mouadine.bilal@gmail.com>
2021-02-03 17:57:32 +0100

    write jsx in app js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,24 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return (
-            <h2 className="btn btn-danger">Reminder App</h2>
+            <div className="App">
+                <img src="" alt=""/>
+                <div className="reminder-title">
+                    <h2>what should u do ?</h2>
+                </div>
+                <input 
+                    className="form-control"
+                    type="text" 
+                    placeholder="Enter what u think .."
+                />
+                <input 
+                    className="form-control"
+                    type="datetime-local" 
+                    placeholder="Enter what u think .."
+                />
+                <button className="btn btn-primary btn-block">Add Reminder</button>
+                <button className="btn btn-danger btn-block">Clear Reminder</button>
+            </div>
         );
     }
 }
