14c04ccd1e2b30d4b248cab7bf58fd296aeb40e6
bilal07 <mouadine.bilal@gmail.com>
2021-02-03 17:23:24 +0100

    create react app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render() {
+        return (
+            <h2>Reminder App</h2>
+        );
+    }
+}
+
+export default App;
